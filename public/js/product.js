42e08a14ac6009f424c4cc401b00c66854305430
t_calvl <lorena.calvo@autodesk.com>
2017-05-02 21:34:42 -0300

    send event click tab

--- public/js/product.js
+++ public/js/product.js
@@ -3,12 +3,25 @@ $(document).ready(function() {
 	$(".product-nav .item-nav").click(function() {
 		var elem = $(this).context;
 		var content = $(elem).attr("value");
+		var name = $(elem).attr("name");
 		if (content === "product-general") {
 			$(".product-technical-data").addClass("hidden");
 			$(".product-general").removeClass("hidden");
+			ga('send', {
+			  hitType: 'event',
+			  eventCategory: 'Product',
+			  eventAction: 'General',
+			  eventLabel: name
+			});
 		} else {
 			$(".product-technical-data").removeClass("hidden");
 			$(".product-general").addClass("hidden");
+			ga('send', {
+			  hitType: 'event',
+			  eventCategory: 'Product',
+			  eventAction: 'DatosTecnicos',
+			  eventLabel: name
+			});
 		}
 		$(".product-nav .item-nav").removeClass("active");
 		$(elem).addClass("active");
